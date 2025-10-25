a417686006ae63ff7b62ed490b8d7dff4d01cf1b
Janderson Gustavo <jeeh2200@gmail.com>
2025-10-25 18:58:02 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FrutasDoSeuZe.Models;
+
+namespace FrutasDoSeuZe.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Fruta> Frutas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // substitua o "postgres" e a senha conforme seu setup
+            optionsBuilder.UseNpgsql("Host=localhost;Database=frutas_db;Username=postgres;Password=JanGustavo083#");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using FrutasDoSeuZe.Data;
+using FrutasDoSeuZe.Models;
+using System.Text;
+
+using var db = new AppDbContext();
+
+bool isRuning = true;
+
+while (isRuning)
+{
+    var menu = new StringBuilder();
+    menu.AppendLine("\n--Frutas do Seu Zé 🍉--");
+    menu.AppendLine("1. Cadastrar fruta");
+    menu.AppendLine("2. Listar frutas");
+    menu.AppendLine("3. Atualizar fruta");
+    menu.AppendLine("4. Deletar fruta");
+    menu.AppendLine("5. Sair");
+    menu.Append("Escolha uma opção: ");
+    Console.Write(menu);
+    string? opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.Write("Nome da fruta: ");
+            string? nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            decimal preco = decimal.Parse(Console.ReadLine()!);
+            Console.Write("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine()!);
+
+            var novaFruta = new Fruta() { Nome = nome, Preco = preco, Quantidade = quantidade };
+            db.Frutas.Add(novaFruta);
+            db.SaveChanges();
+
+            Console.WriteLine($"{nome} cadastrada");
+            break;
+
+
+    }
+
+}
